640edf180c2bfacb0b5604d1d58b0ac602f17a86
Vivek Jadhav <jadhavvivek619@gmail.com>
2021-08-29 20:07:53 +0530

    Add project files.

---
+++ CostExcp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UserDefineException
+{
+    public class CostomExceptio:ApplicationException
+    {
+        public CostomExceptio() : base()
+        {
+
+        }
+        public CostomExceptio(string message) : base(message)
+        {
+
+        }
+    }
+    public class employee
+    {
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if(value >= 20 && value<=59)
+                {
+                    age = value;
+                }
+                else
+                {
+                    CostomExceptio CO = new CostomExceptio("Age must be between 20 to 60");
+                    throw CO;
+                }
+            }
+        }
+        class CostExcp
+        {
+            static void Main(string[] args)
+            {
+                employee emp = new employee();
+                try
+                {
+                    emp.Age = 60;
+                    Console.WriteLine("Age=" + emp.Age);
+                }
+                catch(CostomExceptio CE)
+                {
+                    
+                }
+
+            }
+        }
+    }
+    
+}
